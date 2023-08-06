fe228b8c97c3a222d94edf68925364c21d678f1a
Gustav <gustavmangion@hotmail.com>
2023-08-06 10:25:04 +0400

    Move nextauth variables to env variables

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
 	providers: [
 		GoogleProvider({
-			clientId:
-				process.env.GOOGLE_CLIENT_ID as string,
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
 	],
