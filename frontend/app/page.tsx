50228acdc0986bc200c274618a0f7b8b0c5e9e23
Gustav <gustavmangion@hotmail.com>
2023-08-15 12:31:28 +0400

    Remove code and componenets used during setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import styles from "./page.module.css";
-import Login from "./login";
-import Link from "next/link";
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
 			<div className={styles.description}>Your finance dashboard</div>
-			<Login />
-			<Link href="/dashboard">Dashboard</Link>
 		</main>
 	);
 }
