f0a4dca2c6f6231e0b8a8e31a86fd4ad82a942a8
Gustav <gustavmangion@hotmail.com>
2023-08-20 12:34:07 +0400

    Add redux persist

--- frontend/app/components/provider.tsx
+++ frontend/app/components/provider.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { SessionProvider, useSession } from "next-auth/react";
-import { store } from "../stores/store";
+import { persistor, store } from "../stores/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<Provider store={store}>
-			<SessionProvider>{children}</SessionProvider>
+			<PersistGate loading={null} persistor={persistor}>
+				<SessionProvider>{children}</SessionProvider>
+			</PersistGate>
 		</Provider>
 	);
 }
