2502f8e3a1383d5fe578b6104ead9f468bc9adea
Gustav <gustavmangion@hotmail.com>
2023-08-28 10:40:30 +0400

    Add Base Controller to get userid from token

---
+++ api/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace api.Controllers
+{
+    public class BaseController : Controller
+    {
+        internal string GetUserIdFromToken()
+        {
+            return User.Claims.Where(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").First().Value;
+        }
+    }
+}
