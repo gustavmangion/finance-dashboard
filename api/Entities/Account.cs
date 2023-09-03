3f33cc2b12f9c49a2c31bfb80fa2535e23da511a
Gustav <gustavmangion@hotmail.com>
2023-09-03 15:36:38 +0400

    Add maxlength to account field

--- api/Entities/Account.cs
+++ api/Entities/Account.cs
@@ -1,12 +1,20 @@
-﻿namespace api.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Entities
 {
     public class Account
     {
         public Guid Id { get; set; }
         public string PortfolioId { get; set; }
         public Portfolio Portfolio { get; set; }
+
+        [MaxLength(17)]
         public string AccountNumber { get; set; }
+
+        [MaxLength(45)]
         public string Name { get; set; }
+
+        [MaxLength(45)]
         public string BankName { get; set; }
         public string StatementCode { get; set; }
     }
