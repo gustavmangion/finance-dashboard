941fa258e569ec9d8d6201b8dccf47bf816c7752
Gustav <gustavmangion@hotmail.com>
2023-09-03 19:16:25 +0400

    Add portfolio select

--- frontend/app/upload/createAccount.tsx
+++ frontend/app/upload/createAccount.tsx
@@ -1,6 +1,13 @@
-import { FormLabel, Input } from "@mui/material";
+import {
+	FormLabel,
+	Input,
+	MenuItem,
+	Select,
+	SelectChangeEvent,
+} from "@mui/material";
 import { ChangeEvent, SyntheticEvent, useState } from "react";
 import styles from "../styles/upload.module.scss";
+import { useAppSelector } from "../hooks/reduxHook";
 
 export default function CreateAccount() {
 	const [formState, setFormState] = useState({
@@ -9,6 +16,8 @@ export default function CreateAccount() {
 		password: "",
 	});
 
+	const portfolios = useAppSelector((state) => state.userReducer.portfolios);
+	const defaultPortfolio = portfolios.length === 1 ? portfolios[0].id : "";
 	return (
 		<div className={styles.newAccount}>
 			<h3>Add a new bank account</h3>
@@ -27,11 +36,32 @@ export default function CreateAccount() {
 					value={formState.password}
 					onChange={handleChange}
 				/>
+				<Select
+					name="portfolio"
+					label="Portfolio"
+					onChange={handleSelectChange}
+					defaultValue={defaultPortfolio}
+				>
+					{portfolios.map((x) => {
+						return (
+							<MenuItem key={x.id} value={x.id}>
+								{x.name}
+							</MenuItem>
+						);
+					})}
+				</Select>
 			</form>
 		</div>
 	);
 
 	function handleChange(e: ChangeEvent<HTMLInputElement>) {
+		setFormState({
+			...formState,
+			[e.target.name]: e.target.value,
+		});
+	}
+
+	function handleSelectChange(e: SelectChangeEvent) {
 		setFormState({
 			...formState,
 			[e.target.name]: e.target.value,
