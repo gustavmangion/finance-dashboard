7e5a5add8790d7b1304ca8799bb0e8ebcb9158df
Gustav <gustavmangion@hotmail.com>
2023-09-06 12:18:24 +0400

    Hard coding bank name to account

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -46,6 +46,7 @@ namespace api.Controllers
             newAccount.Name = model.Name;
             newAccount.AccountNumber = model.AccountNumber;
             newAccount.PortfolioId = model.PortfolioId;
+            newAccount.BankName = "ADCB";
             _accountRepository.AddAccount(newAccount);
 
             _accountRepository.SaveChanges();
