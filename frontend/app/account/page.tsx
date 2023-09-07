f4b262eb213da16aa2f9e4031e6b4102160ea0e8
Gustav <gustavmangion@hotmail.com>
2023-09-07 16:14:32 +0400

    Add steps to handling different page views

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useSecurePage } from "../hooks/authHook";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AuthStatus } from "../enums/authStatusEnum";
 import LoadingSkeleton from "../components/loadingSkeleton";
 import { useGetAccountsQuery } from "../apis/base/account/accountService";
@@ -12,6 +12,8 @@ import styles from "../styles/account.module.scss";
 export default function AccountPage() {
 	const authStatus = useSecurePage();
 	const router = useRouter();
+
+	const [view, setView] = useState(PageView.Accounts);
 
 	const { data, isLoading } = useGetAccountsQuery(null);
 
@@ -26,8 +28,18 @@ export default function AccountPage() {
 			<div className="container">
 				<h2>Your Accounts</h2>
 				<div className={styles.accountList}>
-					{isLoading ? <LoadingSkeleton /> : <AccountsList accounts={data!} />}
+					{isLoading ? <LoadingSkeleton /> : getView()}
 				</div>
 			</div>
 		);
+
+	function getView() {
+		if (view === PageView.Accounts) return <AccountsList accounts={data!} />;
+	}
+}
+
+enum PageView {
+	Accounts,
+	Edit,
+	Transactions,
 }
