83b0648f955db531574bbc2b18ea9f722bb720e6
Gustav <gustavmangion@hotmail.com>
2023-09-17 16:27:04 +0400

    Add edit account form fields

--- frontend/app/account/editAccount.tsx
+++ frontend/app/account/editAccount.tsx
@@ -1,4 +1,8 @@
+import { TextField } from "@mui/material";
 import Account from "../apis/base/account/types";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { LoadingButton } from "@mui/lab";
+import materialStyles from "../styles/material.module.scss";
 
 type Props = {
 	account: Account | undefined;
@@ -6,5 +10,47 @@ type Props = {
 };
 
 export default function EditAccount({ account, setAccountToEdit }: Props) {
-	return <p>Edit Account</p>;
+	const [loading, setLoading] = useState(false);
+	const [formState, setFormState] = useState({
+		name: account?.name,
+		accountNumber: account?.accountNumber,
+	});
+
+	return (
+		<>
+			<h2>Editing Account</h2>
+			<h4>Account Name: {account?.name}</h4>
+			<h4>Account Number: {account?.accountNumber}</h4>
+			<form onSubmit={handleSubmit}>
+				<TextField
+					name="name"
+					label="Account Name"
+					variant="standard"
+					value={formState.name}
+					onChange={handleChange}
+					inputProps={{ maxLength: 45 }}
+					required
+				/>
+				<LoadingButton
+					className={materialStyles.primaryButton}
+					type="submit"
+					loading={loading}
+				>
+					Update
+				</LoadingButton>
+			</form>
+		</>
+	);
+
+	function handleChange(e: ChangeEvent<HTMLInputElement>) {
+		setFormState({
+			...formState,
+			[e.target.name]: e.target.value,
+		});
+	}
+
+	function handleSubmit(e: SyntheticEvent<HTMLFormElement>) {
+		e.preventDefault();
+		setLoading(true);
+	}
 }
