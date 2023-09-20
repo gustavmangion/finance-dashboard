ac529c570e9b6f12ab619a14de12a9e391f7d825
Gustav <gustavmangion@hotmail.com>
2023-09-20 16:14:32 +0400

    Add localisation provider and re-enable persistor

--- frontend/app/components/provider.tsx
+++ frontend/app/components/provider.tsx
@@ -4,13 +4,18 @@ import { SessionProvider, useSession } from "next-auth/react";
 import { persistor, store } from "../stores/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import "dayjs/locale/en-gb";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<Provider store={store}>
-			{/* <PersistGate loading={null} persistor={persistor}> */}
-			<SessionProvider>{children}</SessionProvider>
-			{/* </PersistGate> */}
+			<PersistGate loading={null} persistor={persistor}>
+				<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
+					<SessionProvider>{children}</SessionProvider>
+				</LocalizationProvider>
+			</PersistGate>
 		</Provider>
 	);
 }
