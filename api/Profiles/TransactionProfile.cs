76be8248f9923561e4b5e80e2ab12d3ddde176fa
Gustav <gustavmangion@hotmail.com>
2023-09-27 15:36:16 +0400

    Fix issue with epoch conversion on front end

--- api/Profiles/TransactionProfile.cs
+++ api/Profiles/TransactionProfile.cs
@@ -9,16 +9,7 @@ namespace api.Profiles
         public TransactionProfile()
         {
             CreateMap<Transaction, TransactionModel>()
-                .ForMember(
-                    dest => dest.TranDate,
-                    opt =>
-                        opt.MapFrom(
-                            src =>
-                                (
-                                    (DateTimeOffset)src.Date.ToDateTime(new TimeOnly())
-                                ).ToUnixTimeSeconds()
-                        )
-                );
+                .ForMember(dest => dest.TranDate, opt => opt.MapFrom(src => src.Date.ToString()));
         }
     }
 }
