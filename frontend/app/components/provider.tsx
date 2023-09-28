750dd3a4eb0b3d904198a246027e4739b5dce757
Gustav <gustavmangion@hotmail.com>
2023-09-28 15:46:52 +0400

    Add theme provider with palette

--- frontend/app/components/provider.tsx
+++ frontend/app/components/provider.tsx
@@ -7,13 +7,31 @@ import { PersistGate } from "redux-persist/integration/react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import "dayjs/locale/en-gb";
+import { ThemeOptions, ThemeProvider, createTheme } from "@mui/material/styles";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+	const theme = createTheme({
+		palette: {
+			mode: "light",
+			primary: {
+				main: "#1c5d99",
+			},
+			secondary: {
+				main: "#82c0d2",
+			},
+			error: {
+				main: "#c12c0d",
+			},
+		},
+	});
+
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
-					<SessionProvider>{children}</SessionProvider>
+					<SessionProvider>
+						<ThemeProvider theme={theme}>{children}</ThemeProvider>
+					</SessionProvider>
 				</LocalizationProvider>
 			</PersistGate>
 		</Provider>
