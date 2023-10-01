0bceffcdedbeafb5a36d61b15926b16b1529858e
Gustav <gustavmangion@hotmail.com>
2023-10-01 12:01:26 +0400

    Add calls to user service for user sharing

--- frontend/app/apis/base/user/userService.ts
+++ frontend/app/apis/base/user/userService.ts
@@ -1,5 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import User, { CreateUserModel } from "./types";
+import User, {
+	CreateUserModel,
+	CreateUserShare,
+	CreateUserShareCode,
+	UserShareCode,
+	UserShareModel,
+} from "./types";
 import getHeaders from "../headers";
 import { displayError } from "@/app/stores/notificationSlice";
 import { setUser } from "@/app/stores/userSlice";
@@ -12,6 +18,7 @@ export const userApi = createApi({
 			return getHeaders(headers);
 		},
 	}),
+	tagTypes: ["UserShare", "UserShareCode"],
 	endpoints: (builder) => ({
 		getUser: builder.query<User, null>({
 			query: () => `/`,
@@ -47,7 +54,38 @@ export const userApi = createApi({
 				}
 			},
 		}),
+		getUserShares: builder.query<UserShareModel, null>({
+			query: () => `\Share`,
+			providesTags: ["UserShare"],
+		}),
+		addUserShare: builder.mutation({
+			query: (payload: CreateUserShare) => ({
+				url: `\Share`,
+				method: "POST",
+				body: { ...payload },
+			}),
+			invalidatesTags: ["UserShare"],
+		}),
+		getUserShareCode: builder.query<UserShareCode, null>({
+			query: () => `\ShareCode`,
+			providesTags: ["UserShareCode"],
+		}),
+		addOrUpdateUserShareCode: builder.mutation({
+			query: (payload: CreateUserShareCode) => ({
+				url: `\ShareCode`,
+				method: "POST",
+				body: { ...payload },
+			}),
+			invalidatesTags: ["UserShareCode"],
+		}),
 	}),
 });
 
-export const { useGetUserQuery, useAddUserMutation } = userApi;
+export const {
+	useGetUserQuery,
+	useAddUserMutation,
+	useGetUserSharesQuery,
+	useAddUserShareMutation,
+	useGetUserShareCodeQuery,
+	useAddOrUpdateUserShareCodeMutation,
+} = userApi;
