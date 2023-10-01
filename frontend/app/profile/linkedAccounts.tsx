b61b74daff1041329d87ab6816954f6aed020ccc
Gustav <gustavmangion@hotmail.com>
2023-10-01 14:56:08 +0400

    Create linked accounts layout

--- frontend/app/profile/linkedAccounts.tsx
+++ frontend/app/profile/linkedAccounts.tsx
@@ -1,3 +1,42 @@
+import { Alert, Button, CircularProgress } from "@mui/material";
+import { useGetUserSharesQuery } from "../apis/base/user/userService";
+
 export default function LinkedAccounts() {
-	return <div>Linked Accounts</div>;
+	const { isLoading, isFetching, data } = useGetUserSharesQuery(null);
+
+	if (isFetching || isLoading) return <CircularProgress />;
+
+	return (
+		<>
+			<div>
+				<Button>
+					{data?.shareCodeSetup ? "Update code" : "Create new code"}
+				</Button>
+				{data?.shareCodeSetup ? <Button>View code</Button> : null}
+			</div>
+			<div>
+				{!data?.shareCodeSetup ? (
+					<Alert
+						severity="info"
+						sx={{ display: "flex", justifyContent: "center" }}
+					>
+						Setup a code before linking new accounts
+					</Alert>
+				) : data.userShares.length < 1 ? (
+					<Alert
+						severity="info"
+						sx={{ display: "flex", justifyContent: "center" }}
+					>
+						No linked accounts
+					</Alert>
+				) : (
+					getLinkedAccounts()
+				)}
+			</div>
+		</>
+	);
+
+	function getLinkedAccounts() {
+		return <div></div>;
+	}
 }
--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import styles from "../styles/profile.module.scss";
