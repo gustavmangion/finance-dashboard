5878e61c55a7a3810aa93a4a8499744a8ac01483
Gustav <gustavmangion@hotmail.com>
2023-10-04 10:12:57 +0400

    Add currency entity

---
+++ api/Entities/Currency.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Entities
+{
+    public class Currency
+    {
+        public Guid Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public decimal Value { get; set; }
+    }
+}
