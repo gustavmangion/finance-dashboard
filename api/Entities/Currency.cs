4df3553d13c0ecbb3a71bee5da09f5e7535a45c0
Gustav <gustavmangion@hotmail.com>
2023-10-04 10:23:02 +0400

    Add date to currency

--- api/Entities/Currency.cs
+++ api/Entities/Currency.cs
@@ -3,6 +3,7 @@
     public class Currency
     {
         public Guid Id { get; set; }
+        public DateOnly Date { get; set; }
         public string From { get; set; }
         public string To { get; set; }
         public decimal Value { get; set; }
