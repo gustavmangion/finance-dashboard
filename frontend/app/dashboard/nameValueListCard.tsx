8abea9ab35e713cc2967cf8bdb596599fd81d2e8
Gustav <gustavmangion@hotmail.com>
2023-11-20 10:56:01 +0400

    Add expand popup for list

--- frontend/app/dashboard/nameValueListCard.tsx
+++ frontend/app/dashboard/nameValueListCard.tsx
@@ -1,22 +1,36 @@
 import {
+	Button,
 	Card,
 	CardContent,
 	CircularProgress,
 	List,
 	ListItem,
 	ListItemText,
+	Modal,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
 } from "@mui/material";
 import { NameValueModel } from "../apis/base/dashboard/types";
 import styles from "../styles/dashboard.module.scss";
+import materialStyles from "../styles/material.module.scss";
 import NoData from "./noData";
 import LoadError from "./loadError";
 import { getMoneyFormat } from "../helpers/moneyHelper";
+import { useState } from "react";
+import OpenInFullIcon from "@mui/icons-material/OpenInFull";
+import CloseIcon from "@mui/icons-material/Close";
 
 type Props = {
 	title: string;
 	loading: boolean;
 	data: NameValueModel[] | undefined;
 	width?: number;
+	disableExpand?: boolean;
 };
 
 export default function NameValueListCard({
@@ -24,14 +38,22 @@ export default function NameValueListCard({
 	loading,
 	data,
 	width = 1,
+	disableExpand = false,
 }: Props) {
 	let widthClass = styles.normalWide;
 	if (width === 2) widthClass = styles.doubleWide;
 
+	const [expanded, setExpanded] = useState(false);
+
 	return (
 		<Card className={[styles.card, styles.nameValueCard].join(" ")}>
 			<CardContent className={[styles.medium, styles.normalWide].join(" ")}>
-				<h4>{title}</h4>
+				<h4 onClick={handleExpandClick}>
+					{title}
+					{!disableExpand ? (
+						<OpenInFullIcon className={styles.expandIcon} />
+					) : null}
+				</h4>
 				{loading ? (
 					<CircularProgress className={styles.spinner} />
 				) : data === undefined ? (
@@ -42,6 +64,19 @@ export default function NameValueListCard({
 					getList()
 				)}
 			</CardContent>
+			<Modal open={expanded} onClose={() => setExpanded(false)}>
+				<Paper className={[materialStyles.modal, styles.expandList].join(" ")}>
+					<div className={styles.header}>
+						<h3>{title}</h3>
+						<div>
+							<Button size="small" onClick={() => setExpanded(false)}>
+								<CloseIcon />
+							</Button>
+						</div>
+					</div>
+					{getModalList()}
+				</Paper>
+			</Modal>
 		</Card>
 	);
 
@@ -62,6 +97,33 @@ export default function NameValueListCard({
 					</ListItem>
 				))}
 			</List>
+		);
+	}
+
+	function handleExpandClick() {
+		if (!disableExpand && !loading) setExpanded(true);
+	}
+
+	function getModalList() {
+		return (
+			<TableContainer>
+				<Table stickyHeader size="small">
+					<TableHead>
+						<TableRow>
+							<TableCell>Name</TableCell>
+							<TableCell>Amount</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{data!.map((row) => (
+							<TableRow key={row.name}>
+								<TableCell>{row.name}</TableCell>
+								<TableCell>{getMoneyFormat(row.value)}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
 		);
 	}
 }
