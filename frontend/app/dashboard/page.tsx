464a459110f95d02f19adc1a020e717a624db1bf
Gustav <gustavmangion@hotmail.com>
2024-02-27 12:37:00 +0400

    Fix issue with layout content overflow

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -84,101 +84,105 @@ export default function DashboardPage(): React.ReactNode {
 	if (authStatus == AuthStatus.Authorized) {
 		return (
 			<div className="container">
-				<h2>My Dashboard</h2>
-				<h4>All values in base currency {baseCurrency}</h4>
-				<FilterPanel
-					filterState={filterState}
-					portfolios={portfolios}
-					setFilterState={setFilterState}
-					resetFilterState={resetFilterState}
-				/>
-				<div className={styles.mainCardLayout}>
-					<NumberCard
-						title="Total"
-						loading={overviewIsLoading || overviewIsFetching}
-						current={overviewData === undefined ? NaN : overviewData[0].current}
-						previous={
-							overviewData === undefined ? NaN : overviewData![0].previous
-						}
+				<div>
+					<h2>My Dashboard</h2>
+					<h4>All values in base currency {baseCurrency}</h4>
+					<FilterPanel
+						filterState={filterState}
+						portfolios={portfolios}
+						setFilterState={setFilterState}
+						resetFilterState={resetFilterState}
 					/>
-					<NumberCard
-						title="Credit"
-						loading={overviewIsLoading || overviewIsFetching}
-						current={
-							overviewData === undefined ? NaN : overviewData![1].current
-						}
-						previous={
-							overviewData === undefined ? NaN : overviewData![1].previous
-						}
-						noData={
-							overviewData !== undefined &&
-							overviewData[1].current === 0 &&
-							overviewData[2].current === 0
-						}
-					/>
-					<NumberCard
-						title="Debit"
-						loading={overviewIsLoading || overviewIsFetching}
-						current={
-							overviewData === undefined ? NaN : overviewData![2].current
-						}
-						previous={
-							overviewData === undefined ? NaN : overviewData![2].previous
-						}
-						inverseTrend
-						noData={
-							overviewData !== undefined &&
-							overviewData[1].current === 0 &&
-							overviewData[2].current === 0
-						}
-					/>
-					<NumberCard
-						title="Savings"
-						loading={overviewIsLoading || overviewIsFetching}
-						current={
-							overviewData === undefined
-								? NaN
-								: (overviewData![1].current - overviewData[2].current) /
-								  overviewData[1].current
-						}
-						previous={
-							overviewData === undefined
-								? NaN
-								: (overviewData![1].previous - overviewData[2].previous) /
-								  overviewData[1].previous
-						}
-						percentage
-						noData={
-							overviewData !== undefined &&
-							overviewData[1].current === 0 &&
-							overviewData[2].current === 0
-						}
-					/>
-				</div>
-				<div className={styles.cardLayout}>
-					<NameValueListCard
-						title="Spend by Card"
-						loading={cardTotalIsLoading || cardTotalIsFetching}
-						data={cardTotalData}
-					/>
-					<NameValueListCard
-						title="Top Spend"
-						loading={highVenSpendIsLoading || highVenSpendIsFetching}
-						data={highVenSpendData}
-						width={2}
-					/>
-				</div>
-				<div className={styles.cardLayout}>
-					<DonutCard
-						title="Expenses"
-						loading={expBrkIsLoading || expBrkIsFetching}
-						data={expBrkData}
-					/>
-					<LineCard
-						title="Expenses by Date"
-						loading={expDateIsLoading || expDateIsFetching}
-						data={expDateData}
-					/>
+					<div className={styles.mainCardLayout}>
+						<NumberCard
+							title="Total"
+							loading={overviewIsLoading || overviewIsFetching}
+							current={
+								overviewData === undefined ? NaN : overviewData[0].current
+							}
+							previous={
+								overviewData === undefined ? NaN : overviewData![0].previous
+							}
+						/>
+						<NumberCard
+							title="Credit"
+							loading={overviewIsLoading || overviewIsFetching}
+							current={
+								overviewData === undefined ? NaN : overviewData![1].current
+							}
+							previous={
+								overviewData === undefined ? NaN : overviewData![1].previous
+							}
+							noData={
+								overviewData !== undefined &&
+								overviewData[1].current === 0 &&
+								overviewData[2].current === 0
+							}
+						/>
+						<NumberCard
+							title="Debit"
+							loading={overviewIsLoading || overviewIsFetching}
+							current={
+								overviewData === undefined ? NaN : overviewData![2].current
+							}
+							previous={
+								overviewData === undefined ? NaN : overviewData![2].previous
+							}
+							inverseTrend
+							noData={
+								overviewData !== undefined &&
+								overviewData[1].current === 0 &&
+								overviewData[2].current === 0
+							}
+						/>
+						<NumberCard
+							title="Savings"
+							loading={overviewIsLoading || overviewIsFetching}
+							current={
+								overviewData === undefined
+									? NaN
+									: (overviewData![1].current - overviewData[2].current) /
+									  overviewData[1].current
+							}
+							previous={
+								overviewData === undefined
+									? NaN
+									: (overviewData![1].previous - overviewData[2].previous) /
+									  overviewData[1].previous
+							}
+							percentage
+							noData={
+								overviewData !== undefined &&
+								overviewData[1].current === 0 &&
+								overviewData[2].current === 0
+							}
+						/>
+					</div>
+					<div className={styles.cardLayout}>
+						<NameValueListCard
+							title="Spend by Card"
+							loading={cardTotalIsLoading || cardTotalIsFetching}
+							data={cardTotalData}
+						/>
+						<NameValueListCard
+							title="Top Spend"
+							loading={highVenSpendIsLoading || highVenSpendIsFetching}
+							data={highVenSpendData}
+							width={2}
+						/>
+					</div>
+					<div className={styles.cardLayout}>
+						<DonutCard
+							title="Expenses"
+							loading={expBrkIsLoading || expBrkIsFetching}
+							data={expBrkData}
+						/>
+						<LineCard
+							title="Expenses by Date"
+							loading={expDateIsLoading || expDateIsFetching}
+							data={expDateData}
+						/>
+					</div>
 				</div>
 			</div>
 		);
